064bcad6722ca76e72874bd463571fa7a5aabdb6
Karl Mutch <karlmutch@gmail.com>
2015-07-10 10:39:46 -0700

    Stub added to test packaging and unit tests

---
+++ stub_test.go
@@ -0,0 +1,16 @@
+package TestStrawman
+
+import (
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestStrawman(t *testing.T) {
+	word := "strawman"
+	foo, err := Strawman(word)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, true, foo)
+}
